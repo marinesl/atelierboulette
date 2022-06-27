3e2347e747eb9c6a1d333f3db37dd9b4ce0b067c
marinesl <lancelinmarine@gmail.com>
2022-06-27 12:52:40 +0200

    Intégration HTML + FullPage JS + contenus textes et média

---
+++ script/script.js
@@ -0,0 +1,7 @@
+console.log('ok')
+
+// https://github.com/alvarotrigo/fullPage.js/#fullpagejs
+new fullpage('#fullpage', {
+    sectionsColor: ['#8B85B8', 'rgba(139, 133, 184, 0.2)', 'rgba(139, 133, 184, 0.2)', 'rgba(139, 133, 184, 0.2)', 'rgba(139, 133, 184, 0.2)', 'rgba(139, 133, 184, 0.2)', 'rgba(139, 133, 184, 0.2)'],
+    anchors:['accueil', 'paper-art', 'decoration', 'resine', 'upcycling', 'flocage', 'contact'],
+});
